2b0fc359d81c5bc42d508889dc5ebdac4a1a41c6
Enrique Donaire <donaire.q2@gmail.com>
2021-07-22 19:40:47 +0200

    Map Homne

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import MyMap from './components/MyMap'
+import MapView from './components/MapView'
 
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
 
       <Switch>
-      <MyMap/>
+        <MapView/>
             
       </Switch>
     </div>
--- src/components/MapView.js
+++ src/components/MapView.js
@@ -4,16 +4,23 @@ import {MapContainer, TileLayer, Marker, Popup} from  'react-leaflet'
 import  'leaflet/dist/leaflet.css'
 
 
-function MyMap() {
+function MapView() {
     //Some random co-ordinate
 	const position = [51.505, -0.09]
 
     //Do not forget to set a width and height style to your map. Else it won't show up
 	return (
 	<div>
-		Pic for Free Stuff
+		<MapContainer>
+		<TileLayer/>
+		
+			position 
+		<TileLayer/>
+		
+		</MapContainer>
+
 	</div>
 	)
 }
 
-export  default MyMap
+export  default MapView
