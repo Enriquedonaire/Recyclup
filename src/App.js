37c830ba1b0f93f1da8910ff6508eee4fe18b899
Enrique Donaire <donaire.q2@gmail.com>
2021-07-22 17:32:25 +0200

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import * as PATHS from "./utils/paths";
+import MyMap from './components/MyMap'
+
 
 function App() {
   return (
     <div className="App">
+
       <Switch>
-        <Route exact path={PATHS.HOMEPAGE} component={HomePage} /> 
+      <MyMap/>
+            
       </Switch>
     </div>
   );
---
+++ src/components/MyMap.js
@@ -0,0 +1,19 @@
+import React from  'react'
+import {MapContainer, TileLayer, Marker, Popup} from  'react-leaflet'
+// //Don't forget to import the css
+import  'leaflet/dist/leaflet.css'
+
+
+function MyMap() {
+    //Some random co-ordinate
+	const position = [51.505, -0.09]
+
+    //Do not forget to set a width and height style to your map. Else it won't show up
+	return (
+	<div>
+		Pic for Free Stuff
+	</div>
+	)
+}
+
+export  default MyMap
