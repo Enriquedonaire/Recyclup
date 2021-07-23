c3f2875afb00b06a522f9b693710eab6df3c5de6
Sinah Scholz <s.scholz-hehn@outlook.de>
2021-07-23 18:46:28 +0200

    changes in sign in/sign up form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -13,21 +13,21 @@ function SignUp(props) {
     const [username, setUsername] = useState(null)
 
     return (
-        <form onSubmit={(event)=>{onSignUp(email,password)}}>
+        <form onSubmit={(event)=>{onSignUp(email,password, username)}}>
             <div className="form-group">
                 <label htmlFor="InputUsername">Username</label>
-                <input onChange={(event)=>{setUsername(event.target.value)}} type="text" className="form-control" id="InputUsername" name="username" />
+                <input onChange={(event)=>{setUsername(event.target.value)}} type="text" className="form-control" id="InputUsername" name="username" placeholder="Please choose a username"/>
             </div>
             <div className="form-group">
                 <label htmlFor="InputEmail">Email address</label>
-                <input onChange={(event)=>{setEmail(event.target.value)}} type="email" className="form-control" id="InputEmail" name="email" />
+                <input onChange={(event)=>{setEmail(event.target.value)}} type="email" className="form-control" id="InputEmail" name="email" placeholder ="Please fill in your email address."/>
                 <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
             <div className="form-group">
                 <label htmlFor="InputPassword">Password</label>
-                <input onChange={(event)=>{setPassword(event.target.value)}} name="password" type="password" className="form-control" id="InputPassword" />
+                <input onChange={(event)=>{setPassword(event.target.value)}} name="password" type="password" className="form-control" id="InputPassword" placeholder="Please choose a password."/>
             </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
+            <button type="submit" className="btn btn-primary">Sign up!</button>
         </form>
     )
     
