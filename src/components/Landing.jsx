05976ff329b0fc868d3b7670eb7dd8db375203da
Sinah Scholz <s.scholz-hehn@outlook.de>
2021-07-25 10:32:20 +0200

    image list material ui

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,7 +1,3 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
-import axios from 'axios'
-import NavBar from './Navbar'
 
 import { makeStyles } from '@material-ui/core/styles';
 import ImageList from '@material-ui/core/ImageList';
@@ -11,59 +7,57 @@ import ListSubheader from '@material-ui/core/ListSubheader';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
 import itemData from './itemData';
-//und: https://material-ui.com/components/popover/     für das About us Popup. 
+import axios from 'axios'
+import NavBar from './Navbar'
 
-
-
-//material UI "image list with title bars" für die 10 random items 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      justifyContent: 'space-around',
-      overflow: 'hidden',
-      backgroundColor: theme.palette.background.paper,
-    },
-    imageList: {
-      width: 500,
-      height: 450,
-    },
-    icon: {
-      color: 'rgba(255, 255, 255, 0.54)',
-    },
-  }));
-  
-  /**
-   * The example data is structured as follows:
-   *
-   * import image from 'path/to/image.jpg';
-   * [etc...]
-   *
-   * const itemData = [
-   *   {
-   *     img: image,
-   *     title: 'Image',
-   *     author: 'author',
-   *   },
-   *   {
-   *     [etc...]
-   *   },
-   * ];
-   */
- 
- 
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  imageList: {
+    width: 500,
+    height: 450,
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)',
+  },
+}));
+
+/**
+ * The example data is structured as follows:
+ *
+ * import image from 'path/to/image.jpg';
+ * [etc...]
+ *
+ * const itemData = [
+ *   {
+ *     img: image,
+ *     title: 'Image',
+ *     author: 'author',
+ *   },
+ *   {
+ *     [etc...]
+ *   },
+ * ];
+ */
+
+//TODO: show 12 random items with no doubles. 
+//do for loop 15 times inside useEffect [] (?), then push only unique elements to randomelements array? 
+// then slice that new randomelements array 
 
 
-  
-function Landing()  {
+export default function TitlebarImageList() {
+  const classes = useStyles();
 
-const classes = useStyles();
-
-return (
+  return (
     <div className={classes.root}>
       <ImageList rowHeight={180} className={classes.imageList}>
         <ImageListItem key="Subheader" cols={2} style={{ height: 'auto' }}>
-          <ListSubheader component="div">Find nearby items</ListSubheader>
+          <ListSubheader component="div">December</ListSubheader>
         </ImageListItem>
         {itemData.map((item) => (
           <ImageListItem key={item.img}>
@@ -82,8 +76,4 @@ return (
       </ImageList>
     </div>
   );
-} 
-
-       
-
-export default Landing
+}
