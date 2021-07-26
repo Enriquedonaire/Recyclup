07b40d123ca902c82a387dd15e6d2952ea49b1f6
Sinah Scholz <s.scholz-hehn@outlook.de>
2021-07-26 20:54:33 +0200

    did cosmetic changes on SignIN form

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -5,14 +5,14 @@ function Signin(props){
         <form onSubmit={props.onSignin}>
             <div className="form-group">
                 <label htmlFor="InputEmail">Email address</label>
-                <input type="email" className="form-control" id="InputEmail" name="email" />
+                <input type="email" className="form-control" id="InputEmail" name="email" placeholder="Fill in your email adress."/>
             </div>
             <div className="form-group">
                 <label htmlFor="InputPassword">Password</label>
-                <input name="password" type="password" className="form-control" id="InputPassword" />
+                <input name="password" type="password" className="form-control" id="InputPassword" placeholder="Fill in your password."/>
             </div>
             <br></br>
-            <button type="submit" className="btn btn-primary">Submit</button>
+            <button type="submit" className="btn btn-primary">Login</button>
             {
                 props.error ? (
                     <p>{props.error}</p>
